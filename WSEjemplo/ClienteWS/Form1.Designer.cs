332160e85eedf5aff8aebe4fb6699df1352a656a
AbrQ <abrahamqt@gmail.com>
2019-02-10 22:01:49 -0600

    Local Web service example

---
+++ WSEjemplo/ClienteWS/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace ClienteWS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.btnTouch = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Id";
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(97, 43);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(100, 20);
+            this.txtID.TabIndex = 1;
+            // 
+            // btnTouch
+            // 
+            this.btnTouch.Location = new System.Drawing.Point(203, 43);
+            this.btnTouch.Name = "btnTouch";
+            this.btnTouch.Size = new System.Drawing.Size(75, 23);
+            this.btnTouch.TabIndex = 2;
+            this.btnTouch.Text = "Touch me";
+            this.btnTouch.UseVisualStyleBackColor = true;
+            this.btnTouch.Click += new System.EventHandler(this.btnTouch_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(412, 278);
+            this.Controls.Add(this.btnTouch);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Button btnTouch;
+    }
+}
+
---
+++ WSEjemplo/ClienteWS/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClienteWS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTouch_Click(object sender, EventArgs e)
+        {
+            var identificacion = txtID.Text;
+            using (WSPersonas.WSPersonasClient client = new WSPersonas.WSPersonasClient())
+            {
+                var persona = client.obtenerPersona(identificacion);
+                var nombre = persona.Nombre;
+            }
+        }
+    }
+}
