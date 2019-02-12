3f248bc841225b2dc8e0e2e005305552c1fa0f27
AbrQ <abrahamqt@gmail.com>
2019-02-11 22:23:51 -0600

    Numbers

---
+++ Numbers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numbers
+{
+    class Numbers
+    {
+        static void Main(string[] args)
+        {
+            int a = 8;
+            int b = 12;
+            int c = 4;
+
+            int d = (a * b) / c;
+            int e = (a * b) % c;
+
+            Console.WriteLine($"quotient: {d}");
+            Console.WriteLine($"remainder: {e}");
+
+            int maxInt = int.MaxValue;
+            int minInt = int.MinValue;
+
+            Console.WriteLine("Integers are between: " + minInt + " and: " + maxInt);
+
+            double maxDouble = double.MaxValue;
+            double minDouble = double.MinValue;
+
+            Console.WriteLine("Doubles are between: " + minDouble + " and: " + maxDouble);
+
+            Console.ReadKey();
+        }
+    }
+}
