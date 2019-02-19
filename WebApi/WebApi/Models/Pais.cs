7a957848bdaf3cb85807e033b8c4c3153eca088d
AbrQ <abrahamqt@gmail.com>
2019-02-19 11:10:56 -0600

    JWT Web API JSON

---
+++ WebApi/WebApi/Models/Pais.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApi.Models
+{
+    public class Pais
+    {
+
+        public Pais()
+        {
+            Provincias = new List<Provincia>();
+        }
+
+        public int Id { get; set; }
+        [StringLength(30)]
+        public string Nombre { get; set; }
+        [Required]
+        public List<Provincia> Provincias { get; set; }
+    }
+}
+
---
+++ WebApi/WebApi/Models/Provincia.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApi.Models
+{
+    public class Provincia
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        [ForeignKey("Pais")]
+        public int PaisId { get; set; }
+        [JsonIgnore]
+        public Pais Pais { get; set; }
+    }
+}
