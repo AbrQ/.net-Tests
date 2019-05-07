7252bc5af4308f67efc04b8f763bd29cd1e904c0
Abraham <abrahamqt@gmail.com>
2019-05-06 22:59:57 -0500

    TestDayOne

---
+++ VectorsAndMatrix/VectorsAndMatrix/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VectorsAndMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Unidimensional
+            int[] number;
+            number = new int[5];
+
+            //Bidimensional
+            int[,] matrix;
+            matrix = new int[3 , 3];
+
+            for (int row = 0; row < 3; row++)
+            {
+                for (int column = 0; column < 3; column++)
+                {
+                    Console.WriteLine("Give me a number: ");
+                    matrix[row, column] = Convert.ToInt16(Console.ReadLine());
+                }
+            }
+
+            Console.Clear();
+
+            for (int row = 0; row < 3; row++)
+            {
+                for (int column = 0; column < 3; column++)
+                {
+                    Console.Write(" " + matrix[row, column]);
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
