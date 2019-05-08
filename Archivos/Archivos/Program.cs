288d3d8e0014a6b6ebfee3cd41a57069f2e5b592
Abraham <abrahamqt@gmail.com>
2019-05-07 21:36:16 -0500

    Archivos

---
+++ Archivos/Archivos/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Archivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*TextWriter archivo;
+            archivo = new StreamWriter("archivo.txt");
+
+            string mensaje;
+            mensaje = Console.ReadLine();
+
+            archivo.WriteLine(mensaje);
+            archivo.Close();
+
+            Console.Clear();
+            Console.WriteLine("Se guardó el archivo");
+
+            
+
+            TextReader Leer_archivo;
+            Leer_archivo = new StreamReader("archivo.txt");
+
+            Console.WriteLine(Leer_archivo.ReadToEnd());
+            Leer_archivo.Close();*/
+
+            StreamWriter archivo;
+            archivo = File.AppendText("archivo.txt");
+
+            string mensaje;
+
+            mensaje = Console.ReadLine();
+
+            archivo.WriteLine(mensaje);
+
+            archivo.Close();
+
+            Console.ReadKey();
+
+        }
+    }
+}
