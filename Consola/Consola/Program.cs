b8cc980b652af0d605b2a4e83becdc8224721121
AbrQ <abrahamqt@gmail.com>
2019-05-08 16:04:40 -0500

    Clases y método constructor

---
+++ Consola/Consola/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ventana calculadora = new Ventana(); //Instancia y herencia de todos los atributos y metodos de Ventana
+            calculadora.Cerrar();
+
+            Console.ReadKey();
+        }
+    }
+
+    class Ventana
+    {
+        //Atributos
+        private int ancho = 15, alto = 20;
+        private string color = "Rojo";
+
+        //Metodos
+        public void Cerrar()
+        {
+            Console.WriteLine("La ventana se cerró");
+        }
+
+        private void Minimizar()
+        {
+            Console.WriteLine("La ventana se minimizó");
+        }
+
+        public void Maximizar()
+        {
+            Console.WriteLine("La ventana se maximizó");
+        }
+    }
+}
---
+++ MetodoConstructor/MetodoConstructor/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodoConstructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ventana calculadora = new Ventana();
+            //calculadora.Mostrar();
+            Console.ReadKey();
+        }
+    }
+
+    class Ventana{
+
+        //Atributos
+        private int ancho, altura;
+        private string color;
+        //Metodos
+        public Ventana()
+        {
+            ancho = 15;
+            altura = 20;
+            color = "Rojo";
+            Console.WriteLine("El metodo constructor funciona");
+        }
+
+        public void Mostrar()
+        {
+            Console.WriteLine("La dimension de la ventana es: " + ancho + " x " + altura + " y su color es: " + color);
+        }
+
+        ~Ventana() //Metodo destructor
+        {
+            System.Diagnostics.Trace.WriteLine("Se destruyeron los recursos!");
+        }
+    }
+}
