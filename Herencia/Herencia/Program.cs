103008936fc3851fd430ad6233782ff151770ec3
AbrQ <abrahamqt@gmail.com>
2019-05-08 16:26:35 -0500

    Herencia

---
+++ Herencia/Herencia/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hijo herencia = new Hijo();
+            herencia.Saludar();
+            herencia.Despedirse();
+
+            Console.ReadKey();
+        }
+    }
+}
